83a4285fad16fe4d9d4142342707f504e5748aaf
Meyappan Subbaiah <meysubb@gmail.com>
2018-01-04 00:17:24 -0600

    baseline projections by round/position, try pca

--- Post_Season_Draft_Analysis/draft_analysis.R
+++ Post_Season_Draft_Analysis/draft_analysis.R
@@ -1,9 +1,13 @@
-players_df <- readRDS("Post_Season_Draft_Analysis/final_players_data.RDS")
+players <- readRDS("Post_Season_Draft_Analysis/final_players_data.RDS")
 
 ### Basic plot before develop ranks
-players_df <- players_df %>% mutate(
-  diff = PTS - exp_pts
-)
+players_df <- players %>% mutate(
+  diff = PTS - exp_pts,
+  round = ceiling(draft_pos/10)
+) %>% select(-`TEAM POS`)
+
+players_df <- players_df[-2,]
+players_df <- players_df[-12,]
 
 labels <- players_df %>% filter(diff>50)
 library(ggplot)
@@ -17,3 +21,36 @@ ggplot(players_df,aes(x=exp_pts,y=PTS,color=playerposition)) +
          subtitle="2017 Season - ESPN",
        caption="@msubbaiah1") +
   theme_bw()
+
+### Baseline Projections by Position (drafted players only)
+
+projected_by_position <- players_df %>% group_by(playerposition) %>% 
+  summarize(players_drafted=n(),
+            avg_projected = mean(exp_pts),
+            avg_value = mean(PTS),
+            avg_diff = mean(diff)) %>% ungroup()
+
+projected_by_round <- players_df %>% group_by(round) %>% 
+  summarize(players_drafted=n(),
+            avg_projected = mean(exp_pts),
+            avg_value = mean(PTS),
+            avg_diff = mean(diff)) %>% ungroup()
+
+###
+q <- players_df %>% mutate(
+  val = diff/round,
+  val2 = diff/draft_pos
+) 
+
+### PCA 
+pca_train <- players_df %>% select(draft_pos,exp_pts,PTS)
+prin_comp <- prcomp(pca_train, scale. = T)
+
+library(ggfortify)
+library(ggrepel)
+biplot(prin_comp,xlabs=players_df[,3])
+
+autoplot(prin_comp, data = players_df, colour = 'playerposition',label=T,shape=F) 
+  
+
+
