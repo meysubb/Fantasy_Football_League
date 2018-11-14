58c72556fffd8a460b3ab1ce9b39a714eb1d3b47
meysubb <meysubb@gmail.com>
2018-11-14 09:49:07 -0600

    lunch work yday

--- Playoff_Chances/playoff_chance_sim.R
+++ Playoff_Chances/playoff_chance_sim.R
@@ -2,12 +2,16 @@ library(tidyverse)
 options(stringsAsFactors = FALSE)
 standings <- read_csv("sleeper_current_standings.csv")
 
+standings <- standings %>% mutate(
+  avg_pts = (fpts + fpts_decimal/100)/(wins+losses)
+)
+
 
 playoff_df <- as.data.frame(standings$name)
 playoff_df$made <- 0
 colnames(playoff_df)[1] <- "Team"
 
-score_std_dev <- sd(standings$ppts)
+score_std_dev <- sd(standings$avg_pts)
 
 sched <- read_csv("r_sched_2.csv")
 sched <- sched %>% mutate(
@@ -17,12 +21,12 @@ sched <- sched %>% mutate(
 #sched <- sched %>% select(-X4,-X5)
 
 library(foreach)
-n <- 50
+n <- 5000
 pb <- txtProgressBar(0, n, style = 2)
 ## Generate random df scores
-t <- foreach(i=1:50) %do% {
+t <- foreach(i=1:5000) %do% {
   setTxtProgressBar(pb, i)
-  scores <- as.data.frame(replicate(3,rnorm(10, mean = standings$ppts, sd = score_std_dev)))
+  scores <- as.data.frame(replicate(3,rnorm(10, mean = standings$avg_pts, sd = score_std_dev)))
   scores$Team <- standings$name
   ## Need to find a better way to extract this
   colnames(scores)[1:3] <- c(11,12,13)
@@ -46,18 +50,18 @@ t <- foreach(i=1:50) %do% {
               points_scored = sum(value)) %>% ungroup()
   
   
-  ext_standings <- inner_join(standings,sim_record)
+  ext_standings <- inner_join(standings,sim_record,by=c("name"="Team"))
   ext_standings <- ext_standings %>% 
-    mutate(update_W = W + sim_win,
-           update_L = L + sim_loss,
+    mutate(update_W = wins + sim_win,
+           update_L = losses + sim_loss,
            win_pct = update_W/(update_L+update_W),
-           upd_pts_scored = `Pts For` + points_scored) %>% 
+           upd_pts_scored = ppts + points_scored) %>% 
     arrange(-win_pct,-upd_pts_scored) %>% 
     mutate(rank= row_number(),
-           playoffs = ifelse(rank<=7,1,0)) 
+           playoffs = ifelse(rank<=6,1,0)) 
   
-  playoff_odds <- ext_standings %>% select(Team,playoffs)
-  inner_join(playoff_df,playoff_odds)
+  playoff_odds <- ext_standings %>% select(name,playoffs)
+  inner_join(playoff_df,playoff_odds,by=c("Team"="name"))
 }
 
 
