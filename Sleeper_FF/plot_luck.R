fd874aa461f55ddf33e3df78c50fa3e5de0b35a4
meysubb <meysubb@gmail.com>
2018-11-20 16:42:34 -0600

    lunch break ff

--- Sleeper_FF/plot_luck.R
+++ Sleeper_FF/plot_luck.R
@@ -9,6 +9,14 @@ final_proj <- as.data.frame(final_proj)
 
 melt_plot_dat <- reshape2::melt(final_proj,id="X1")
 
+grid <- with(melt_plot_dat, seq(min(value), max(value), length.out = 150))
+dens <- plyr::ddply(melt_plot_dat, "variable", function(x){
+  data.frame(
+    value = grid,
+    density = dnorm(grid, mean(x$value), sd(x$value))
+  )
+})
+
 ggplot(melt_plot_dat,aes(x=X1,y=value)) + 
   geom_bar(stat='identity') + 
   facet_wrap(~variable) + 
