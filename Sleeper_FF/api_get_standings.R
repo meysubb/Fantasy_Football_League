8bbbb154dafcd9005b78bc01634e42404aa87f49
mreyers <matthewreyers3333@gmail.com>
2020-09-09 22:34:42 -0700

    Data and some additional functionality

--- Sleeper_FF/api_get_standings.R
+++ Sleeper_FF/api_get_standings.R
@@ -1,6 +1,9 @@
+# Zelus fantasy league
+
 library(tidyverse)
 library(httr)
-url <- "https://api.sleeper.app/v1/league/307655088971718656/rosters"
+base_url <- "https://api.sleeper.app/v1/league/604138427104952320/"
+url <- paste0(base_url, "rosters") 
 
 
 r_get <- GET(url)
@@ -16,7 +19,7 @@ s_clean <- lapply(s_content,extract_standings_info)
 standings_df <- bind_rows(lapply(s_clean,as.data.frame.list))
 
 
-users_url <- "https://api.sleeper.app/v1/league/307655088971718656/users"
+users_url <- paste0(base_url, "users")
 
 us_get <- GET(users_url)
 us_content <- content(us_get)
@@ -35,3 +38,82 @@ user_df <- bind_rows(lapply(user_names, as.data.frame.list))
 
 final_df <- standings_df %>% inner_join(user_df)
 write_csv(final_df,"sleeper_current_standings.csv")
+
+rosters_url <- paste0(base_url, "rosters")
+
+rost_get <- GET(rosters_url)
+rost_content <- content(rost_get)
+
+extract_rosters <- function(lst){
+  user_id <- lst$owner_id
+  players <- lst$players
+  plays <- rbind(players)
+  starters <- lst$starters
+  starts <- rbind(starters)
+  
+  return(tibble(user = user_id, starters = list(starters), players = list(players)))
+}
+
+rosters <- lapply(rost_content, extract_rosters)
+rosters_df <- bind_rows(rosters) 
+
+# Make a quick function that modifies players to be a df in which col1 is player, col2 is starter/not
+fix_fn <- function(start, play){
+  starters <- do.call(rbind.data.frame, start)
+  names(starters) <- "starters"
+  
+  players <- do.call(rbind.data.frame, play)
+  names(players) <- "players"
+  
+  players <- players %>%
+    mutate(is_starter = players %in% starters$starters)
+  
+  return(players)
+}
+
+hold <- rosters_df %>% slice(1) %>% 
+  mutate(fix = map2(starters, players, fix_fn))
+
+# Do these IDs match nflfastR?
+seasons <- 2010:2019
+pbp <- purrr::map_df(seasons, function(x) {
+  readRDS(
+    url(
+      glue::glue("https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_{x}.rds")
+    )
+  )
+})
+
+# No, have to name match which is kind of lame
+all_players_url <- "https://api.sleeper.app/v1/players/nfl"
+all_players <- GET(all_players_url)
+all_players_cont <- content(all_players)
+
+# I can probably convert this to tibble
+players_tibble <- tibble(data = all_players_cont) %>%
+  unnest_wider(data) %>%
+  unnest_wider(fantasy_positions)
+
+# Rename positions
+players_tibble <- players_tibble %>%
+  rename("primary_position" = `...1`, 
+         "secondary_position" = `...2`,
+         "tertiary_position" = `...3`)
+saveRDS(players_tibble, "players_and_ids.rds")
+
+# Can I slice off the first set of digist in the pbp id's and join with sportsradar_id?
+mod_pbp <- pbp %>%
+  select(passer_player_id, passer_player_name) %>%
+  group_by(passer_player_id, passer_player_name) %>%
+  slice(1) %>%
+  ungroup() %>%
+  mutate(passer_player_id = str_remove(passer_player_id, "[0-9]+\\-"))
+
+mod_sleeper <- players_tibble %>%
+  filter(position %in% "QB") %>%
+  mutate(adj_sportradar = str_remove(sportradar_id, "[0-z]+\\-")) %>%
+  select(full_name, adj_sportradar)
+
+# Fails :(
+mod_pbp %>%
+  inner_join(mod_sleeper, by = c("passer_player_id" = "adj_sportradar"))
